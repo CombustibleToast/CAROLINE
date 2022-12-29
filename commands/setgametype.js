750575b47a5042d7d0be6de60bd9a2e6e6dde734
combustibletoast <robert.sobert24@gmail.com>
2022-12-28 22:17:30 -0500

    Initial. Will be used to manually change a games type along with lib/gameFunctions/gameSetType.js

---
+++ commands/setgametype.js
@@ -0,0 +1,45 @@
+const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder } = require("discord.js");
+const { officerCheck, fetchGameFile } = require("../lib/common");
+
+module.exports = {
+    //command definition for discord API
+    data: new SlashCommandBuilder()
+        .setName('setgametype')
+        .setDescription('Manually set the type of the game associated with this channel.'),
+
+    //the funciton to be run
+    async execute(interaction) {
+        if (!officerCheck()) {
+            await interaction.reply({ content: "You are not authorized to use this command.", ephemeral: true });
+            return;
+        }
+
+        const gameData = fetchGameFile(interaction.channel.id);
+        if (!gameData) {
+            await interaction.reply({ content: "This channel does not seem to be associated with a game. (File not found)", ephemeral: true });
+            return;
+        }
+
+        const menuRow = new ActionRowBuilder().addComponents(
+            new StringSelectMenuBuilder()
+                .setCustomId(`gameSetType${gameData.channelId}`)
+                .setPlaceholder("Nothing selected")
+                .addOptions(
+                    {
+                        label: "Campaign",
+                        value: "c"
+                    },
+                    {
+                        label: "Series",
+                        value: "s"
+                    },
+                    {
+                        label: "Oneshot",
+                        value: "o"
+                    }
+                )
+        );
+
+        await interaction.reply({content: "Select the game's new type.", components: [menuRow], ephemeral: true});
+    }
+}
