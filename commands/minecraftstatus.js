2f530b1923a13bef36abeb4523cb491bc140a191
combustibletoast <robert.sobert24@gmail.com>
2023-02-14 17:17:52 -0500

    New command, checks if the minecraft server is up

---
+++ commands/minecraftstatus.js
@@ -0,0 +1,53 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    //command definition for discord API
+    data: new SlashCommandBuilder()
+        .setName('minecraftstatus')
+        .setDescription('Check whether tosat.ddns.net is online.'),
+
+    //the funciton to be run
+    async execute(interaction) {
+        const fetch = require('node-fetch');
+
+        await interaction.deferReply();
+
+        let url = "https://api.mcsrvstat.us/2/tosat.ddns.net";
+        let settings = { method: "Get" };
+        await fetch(url, settings)
+            .then(response => response.json())
+            .then((json) => {
+                let embed;
+                if(json.online)
+                    embed = buildOnlineEmbed(json);
+                else
+                    embed = buildOfflineEmbed(json);
+                interaction.followUp({embeds: [embed]});
+            })
+            .catch(() => {
+                interaction.followUp("API failure; the website didn't respond.")
+            });
+        
+        //interaction.followUp("Recieved a reply.");
+    }
+}
+
+function buildOfflineEmbed(data){
+    const embed = new EmbedBuilder()
+        .setColor("aa2211")
+        .setTitle("Server is Currently Offline")
+        .setDescription("The server is currently offline.\nIt's either undergoing maintenence or there isn't a world currently.");
+    return embed;
+}
+
+function buildOnlineEmbed(data){
+    const playersOnlineStatus = data.players.online = 0 ? "Nobody is currently playing." : `Currently online: ${data.players.list}`;
+    const modsStatus = `Mods: ${data.mods ? "Yes" : "Nope"}`;
+
+    let description = `${playersOnlineStatus}\n${modsStatus}`;
+    const embed = new EmbedBuilder()
+        .setColor("22ee66")
+        .setTitle("The Server is Online!")
+        .setDescription(description);
+    return embed;
+}
