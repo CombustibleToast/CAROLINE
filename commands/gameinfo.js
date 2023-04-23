24ddb4664a179b9fdc92ed69ab0d0c76ca68f59f
combustibletoast <robert.sobert24@gmail.com>
2023-04-22 23:18:48 -0400

    Added, worked basically on the first try :sunglasses:

---
+++ commands/gameinfo.js
@@ -0,0 +1,66 @@
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const { officerCheck, fetchGameFile } = require('../lib/common.js');
+const internal = require("stream");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('gameinfo')
+        .setDescription("Queries a game for info.")
+        .addChannelOption(option =>
+            option.setName('gamechannel')
+                .setRequired(true)
+                .setDescription("The channel of the game in question.")
+        ),
+
+    async execute(interaction) {
+        //only officers may use this command
+        if (!officerCheck(interaction.member)) {
+            await interaction.reply({ content: "You are not authorized to perform this action.", ephemeral: true });
+            return;
+        }
+
+        //collect game id
+        const gameId = interaction.options.getChannel('gamechannel').id;
+
+        //get game data
+        const gameData = fetchGameFile(gameId);
+
+        //check if the game exists/is imported
+        if (!gameData) {
+            await interaction.reply({ content: "The channel specified isn't a game or hasn't been imported by Caroline.", ephemeral: true })
+            return;
+        }
+
+        await interaction.deferReply();
+
+        //compile data
+        const data = `
+        Game Name: ${gameData.gameName}\n
+        Type: ${gameData.gameType}\n
+        System: ${gameData.gameSystem}\n
+        Channel: ${interaction.guild.channels.cache.get(gameData.channelId)}\n
+        Role: ${interaction.guild.roles.cache.get(gameData.roleId)}\n
+        Status: ${gameData.status}\n
+        Joinability: ${gameData.joinability}\n
+        GM: ${interaction.guild.members.cache.get(gameData.gmId)}\n
+        Players: ${getPlayerList(interaction, gameData)}\n
+        `
+
+        //build embed
+        const embed = new EmbedBuilder()
+            .setColor(`DarkGreen`)
+            .setTitle(`Info on ${gameData.gameName}`)
+            .setDescription(data)
+
+        //send
+        await interaction.followUp({ embeds: [embed] })
+    }
+}
+
+function getPlayerList(interaction, gameData){
+    let playerList = "";
+    for(player of gameData.participants){
+        playerList += `${interaction.guild.members.cache.get(player)}, `
+    }
+    return playerList
+}
