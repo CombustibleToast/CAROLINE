01b91b383dd87e3f0a89a457d2a5bd5f723d83a8
combustibletoast <robert.sobert24@gmail.com>
2023-05-18 20:07:54 -0400

    Now will call enqueue function then resume playback.

--- commands/music commands/play.js
+++ commands/music commands/play.js
@@ -4,21 +4,31 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("play")
         .setDescription("Play a Youtube video or playlist given a URL or search query.")
-        .addStringOption(option => 
+        .addStringOption(option =>
             option
                 .setName("query")
                 .setDescription("Enter a Youtube video or playlist URL, or enter a search query.")
                 .setRequired(false)
         ),
 
-        async execute(interaction){
-            /*
-            if(!interaction.member.voice.channel){
-                await interaction.reply({content: "You need to be in a voice channel to use music commands.", ephemeral: true});
-                return;
-            }*/
-            
-            //CAROLINE api entrance
-            interaction.client.functions.get("play").execute(interaction);
+    /**
+    * This function should:
+    * Assign a new vacant loudspeaker to the user's channel if there isn't one already already assigned and have it join the channel.
+    * If there is no queue, create a new one.
+    * Parse whether the query is a video link, playlist link, or search term and enqueue the found video(s).
+    * If there is no query, unpause the playback (call resume.js).
+    * If there is a query and the queue was previously empty, start playback.
+    */
+    async execute(interaction) {
+        if (!interaction.member.voice.channel) {
+            await interaction.reply({ content: "You need to be in a voice channel to use music commands.", ephemeral: true });
+            return;
         }
+
+        let response = await interaction.client.functions.get("enqueue").execute(interaction);
+
+        await interaction.editReply(`${response}\n\nResuming/starting playback. (Use /enqueue to avoid this in the future.)`);
+
+        //TODO: resume/start playback
+    }
 }
