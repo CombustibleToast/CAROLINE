012a290380845a1135bdc252e1d6c96934c0eaf5
CombustibleToast <robert.sobert24@gmail.com>
2023-06-03 14:41:34 -0400

    Transitioned frontend responses to function.

--- commands/music commands/join.js
+++ commands/music commands/join.js
@@ -7,18 +7,6 @@ module.exports = {
         .setDescription('Assigns a loudspeaker to your channel.'),
 
     async execute(interaction) {
-        await interaction.deferReply({ ephemeral: true });
-        //attempt to connect a loudspeaker
-        const result = await interaction.client.functions.get("connectLoudspeaker").execute(interaction);
-
-        //respond to the user
-        if (result.status == "no vacancies")
-            await interaction.followUp({ content: "Unfortunately there are no vacant loudspeakers at this time.", ephemeral: true });
-
-        if (result.status == "already assigned")
-            await interaction.followUp({ content: "There is already a loudspeaker in your channel.", ephemeral: true });
-
-        if (result.status == "new assignment")
-            await interaction.followUp({ content: "Assigned a loudspeaker to your channel.", ephemeral: true });
+        await interaction.client.functions.get("join").execute(interaction);
     }
 }
