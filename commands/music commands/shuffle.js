e185b18ef1717ad1820151cbdbd47ba00b6a647a
CombustibleToast <robert.sobert24@gmail.com>
2023-06-03 15:12:54 -0400

    Transitioned frontend responses to function.

--- commands/music commands/shuffle.js
+++ commands/music commands/shuffle.js
@@ -5,15 +5,7 @@ module.exports = {
         .setName("shuffle")
         .setDescription("Shuffles the playlist."),
 
-    async execute(interaction) {
-        if (!interaction.member.voice.channel) {
-            await interaction.reply({ content: "You need to be in a voice channel to use music commands.", ephemeral: true });
-            return;
-        }
-
-        await interaction.deferReply({ephemeral: true});
-
-        const response = await interaction.client.functions.get("shuffleQueue").execute(interaction);
-        await interaction.followUp(response);
+    async execute(interaction) { 
+        await interaction.client.functions.get("shuffleQueue").execute(interaction);
     }
 }
