3ceef2e62560ab8de1e0f605918f7130811ddc6b
CombustibleToast <robert.sobert24@gmail.com>
2023-06-03 14:14:55 -0400

    Transitioned frontend responses to function.

--- commands/music commands/loop.js
+++ commands/music commands/loop.js
@@ -3,21 +3,13 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("loop")
-        .setDescription("Sets the playlist on a loop. If not given a choice, it will be toggled.")
+        .setDescription("Sets the playlist on a loop. If not given a choice, on/off will be toggled.")
         .addBooleanOption(option =>
             option
                 .setName('on')
                 .setDescription("True turns loop on, False turns loop off.")),
 
     async execute(interaction) {
-        if (!interaction.member.voice.channel) {
-            await interaction.reply({ content: "You need to be in a voice channel to use music commands.", ephemeral: true });
-            return;
-        }
-
-        await interaction.deferReply({ephemeral: true});
-
-        const response = await interaction.client.functions.get("loop").execute(interaction);
-        await interaction.followUp(response);
+        await interaction.client.functions.get("loop").execute(interaction);
     }
 }
