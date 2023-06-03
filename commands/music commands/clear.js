1a1b5cea2cf72280957a7043971bfd9402f217aa
CombustibleToast <robert.sobert24@gmail.com>
2023-06-03 12:39:52 -0400

    Transitioned all frontend (replies) from the command to the function.

--- commands/music commands/clear.js
+++ commands/music commands/clear.js
@@ -11,14 +11,6 @@ module.exports = {
                 .setRequired(false)),
 
     async execute(interaction) {
-        if (!interaction.member.voice.channel) {
-            await interaction.reply({ content: "You need to be in a voice channel to use music commands.", ephemeral: true });
-            return;
-        }
-
-        await interaction.deferReply({ephemeral: true});
-
-        const response = await interaction.client.functions.get("clearQueue").execute(interaction);
-        await interaction.followUp(response);
+        await interaction.client.functions.get("clearQueue").execute(interaction);
     }
 }
