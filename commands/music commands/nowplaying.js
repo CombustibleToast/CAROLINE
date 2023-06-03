d857f0598e23d9aa49cf36a7a12259c40566a52a
CombustibleToast <robert.sobert24@gmail.com>
2023-06-03 14:18:56 -0400

    Transitioned frontend responses to function.

--- commands/music commands/nowplaying.js
+++ commands/music commands/nowplaying.js
@@ -6,14 +6,6 @@ module.exports = {
         .setDescription("Displays the current song."),
 
     async execute(interaction) {
-        if (!interaction.member.voice.channel) {
-            await interaction.reply({ content: "You need to be in a voice channel to use music commands.", ephemeral: true });
-            return;
-        }
-
-        await interaction.deferReply({ephemeral: true});
-
-        const response = await interaction.client.functions.get("nowPlaying").execute(interaction);
-        await interaction.followUp(response);
+        await interaction.client.functions.get("nowPlaying").execute(interaction);
     }
 }
