1e300be0cd6224b9ba4dbe1de615f6645b9fe243
CombustibleToast <robert.sobert24@gmail.com>
2023-06-03 15:17:08 -0400

    Transitioned frontend responses to function.

--- commands/music commands/skip.js
+++ commands/music commands/skip.js
@@ -10,14 +10,6 @@ module.exports = {
                 .setDescription("(Default is false/no) Should the next song be paused?")),
 
     async execute(interaction) {
-        if (!interaction.member.voice.channel) {
-            await interaction.reply({ content: "You need to be in a voice channel to use music commands.", ephemeral: true });
-            return;
-        }
-
-        await interaction.deferReply({ephemeral: true});
-
-        const response = await interaction.client.functions.get("skip").execute(interaction);
-        await interaction.editReply(response);
+        await interaction.client.functions.get("skip").execute(interaction);
     }
 }
