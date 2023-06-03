734a5b959a4899bb3c526124906165d4f51d3692
CombustibleToast <robert.sobert24@gmail.com>
2023-06-03 15:08:04 -0400

    Transitioned frontend responses to function.

--- commands/music commands/resume.js
+++ commands/music commands/resume.js
@@ -6,14 +6,6 @@ module.exports = {
         .setDescription("Resumes playback."),
 
     async execute(interaction) {
-        if (!interaction.member.voice.channel) {
-            await interaction.reply({ content: "You need to be in a voice channel to use music commands.", ephemeral: true });
-            return;
-        }
-
-        await interaction.deferReply({ephemeral: true});
-
-        const response = await interaction.client.functions.get("resumePlayback").execute(interaction);
-        await interaction.editReply(response);
+        await interaction.client.functions.get("resumePlayback").execute(interaction);
     }
 }
